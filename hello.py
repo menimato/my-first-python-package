c047dfeb2766e6a55c9e09ebba910715cc389017
Bruno M. Matosak <brunomenini10@gmail.com>
2024-12-05 13:53:17 +0000

    Added help message

--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
 def hello(name = "world"):
+  """hello
+  
+  Prints a brief charming message greeting a person.
+  
+  Parameters
+  ----------
+  name : str
+    Name of the user/person to be greeted.
+
+  Returns
+  -------
+  None.
+  
+  Examples
+  --------
+  >>> hello('Bruno')
+  """
   print("Hello ", name, "! Welcome to my first Python package!", sep = "")
